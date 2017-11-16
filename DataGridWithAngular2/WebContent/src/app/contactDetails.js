1f4a4ca3b87402be5caa83d173e27b569dbcc9fd
pradeep.pandhi <pradeep.pandhi@HYDPCM308345D.ad.infosys.com>
2017-11-16 10:17:50 +0530

    Removed unused files

--- DataGridWithAngular2/WebContent/src/app/contactDetails.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ContactDetails = (function () {
-    function ContactDetails() {
-    }
-    return ContactDetails;
-}());
-exports.ContactDetails = ContactDetails;
-//# sourceMappingURL=contactDetails.js.map
